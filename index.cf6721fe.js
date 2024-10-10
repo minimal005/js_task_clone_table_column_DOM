dbd938428fb6686fa27a65c24c695b403efe64e3
Halyna <minimal005@gmail.com>
2024-10-10 17:29:13 +0300

    make build

---
+++ index.cf6721fe.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelector("table").rows).forEach(function(e){var l=e.cells[1].cloneNode(!0),c=e.cells[4],r=document.createElement(e.cells[0].localName);e.append(r),e.cells[4].replaceWith(l),e.cells[5].replaceWith(c)});
+//# sourceMappingURL=index.cf6721fe.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const table = document.querySelector("table");
+const changeTable = Array.from(table.rows);
+changeTable.forEach((el)=>{
+    const newColumn = el.cells[1].cloneNode(true);
+    const lastElementOfColumn = el.cells[4];
+    const td = document.createElement(el.cells[0].localName);
+    el.append(td);
+    el.cells[4].replaceWith(newColumn);
+    el.cells[5].replaceWith(lastElementOfColumn);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
